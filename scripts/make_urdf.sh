dbd2d06acebe450abb5e51cb761ffe49a644c472
Norton Kelly-Boxall <norton.kellyboxall@hdr.qut.edu.au>
2018-09-04 09:45:09 +1000

    Added two dof head
    Made the necessary changes that I know in relation to allowing Movit and Rviz to display the head.

--- scripts/make_urdf.sh
+++ scripts/make_urdf.sh
@@ -12,8 +12,9 @@ echo "** Creating URDF file from gummi_description_base.urdf.xacro and gummi_des
 
 export GUMMIBASEPATH=${PWD}/../gummi_base_${ROS_GUMMI_BASE}
 export GUMMIEEPATH=${PWD}/../gummi_ee_${ROS_GUMMI_EE}
-rosrun xacro xacro --inorder ${GUMMIBASEPATH}/xacro/gummi_description.urdf.xacro base:=${GUMMIBASEPATH} ee:=${GUMMIEEPATH} > ${GUMMIBASEPATH}/urdf/gummi_description.urdf
+export GUMMIHEADPATH=${PWD}/../two_dof_head
+rosrun xacro xacro --inorder ${GUMMIBASEPATH}/xacro/gummi_description.urdf.xacro base:=${GUMMIBASEPATH} ee:=${GUMMIEEPATH} twodofhead:=${GUMMIHEADPATH} > ${GUMMIBASEPATH}/urdf/gummi_description.urdf
 
 echo "** Creating SRDF file from gummi_base.srdf.xacro and gummi_ee.srdf.xacro."
 
-rosrun xacro xacro --inorder ${GUMMIBASEPATH}/xacro/gummi.srdf.xacro base:=${GUMMIBASEPATH} ee:=${GUMMIEEPATH} > ${GUMMIBASEPATH}/config/gummi.srdf
+rosrun xacro xacro --inorder ${GUMMIBASEPATH}/xacro/gummi.srdf.xacro base:=${GUMMIBASEPATH} ee:=${GUMMIEEPATH} twodofhead:=${GUMMIHEADPATH} > ${GUMMIBASEPATH}/config/gummi.srdf
